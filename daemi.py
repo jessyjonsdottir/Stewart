2e5c5b43eb2362ea17cf653207f3241797d0652f
Kjartan Magnússon <kjartan@vr2t-dw496.rhi.hi.is>
2019-02-24 15:37:33 +0000

    ja

--- daemi.py
+++ daemi.py
@@ -1,11 +1,15 @@
 import numpy as np
+import math as m
+import matplotlib.pyplot as plt
 
 #Fastar stærðir: L1, L2, L3, p1, p2, p3
-#Viljum finna theta, x, y sem er þá afleiddar stærðir þeirra gefnu
+#Viljum að forritið taki inn theta
+#Viljum að forritið skili x, y
 def f(theta):
+    pi = m.pi
     #Hliðarlengdir Stewart-pallsins
     L1 = 2
-    L2 = sqrt(2)
+    L2 = m.sqrt(2)
     L3 = L2
     gamma = pi/2
 
@@ -13,29 +17,29 @@ def f(theta):
     x0 = 0
     y0 = 0
 
-    x1 = 4
+    x1 = 0
     y1 = 0
 
     x2 = 0
-    y2 = 4
+    y2 = 0
 
 #Hér þarf að finna þeta fyrst
-    A2 = L3*cos(theta) - x1
-    B2 = L3*sin(theta)
-    A3 = L2*cos(theta+gamma) - x2
-    B3 = L2*sin(theta+gamma) - y2
+    A2 = L3*m.cos(theta) - x1
+    B2 = L3*m.sin(theta)
+    A3 = L2*m.cos(theta+gamma) - x2
+    B3 = L2*m.sin(theta+gamma) - y2
 
     #x = p1*cos(theta)
     #y = p1*sin(theta)
     #p1 = sqrt(x**2 + y**2)
     #p2 = sqrt((x+A2)**2 + (y+B2)**2)
     #p3 = sqrt((x+A3)**2 + (y+B3)**2)
-    p1 = sqrt(5)
+    p1 = m.sqrt(5)
     p2 = p1
     p3 = p1
 
     #Hvað er þetta
-    D = 2(A2*B3 - B2*A3)
+    D = 2*(A2*B3 - B2*A3)
     #D má ekki vera 0:
     #if D != 0:
         #x = (B3(p2**2-p1**2-A2**2-B2**2)-B2(p3**2-p1**2-A3**2-B3**2))/D
@@ -43,6 +47,13 @@ def f(theta):
 
     #else
         #return("Ógilt")
-    N1 = B3(p2**2-p1**2-A2**2-B2**2)-B2(p3**2-p1**2-A3**2-B3**2)
-    N2 = -A3(p2**2-p1**2-A2**2-B2**2)+A2(p3**2-p1**2-A3**2-B3**2)
+    N1 = B3*(p2**2-p1**2-A2**2-B2**2)-B2*(p3**2-p1**2-A3**2-B3**2)
+    N2 = -A3*(p2**2-p1**2-A2**2-B2**2)+A2*(p3**2-p1**2-A3**2-B3**2)
     return N1**2 + N2**2 -p1**2*D**2
+
+def teikna()
+    x = np.linspace(-math.pi,math.pi,100)
+    # 100 linearly spaced numbers frá -pi til pi
+    y = f(x)
+    plt.plot(x, y)
+    plt.show()

5fd1b8ab146d28cee621edc4ea26f60d821374ab
Ívar Dór Orrason <ivardor@vr2t-dw025.rhi.hi.is>
2019-02-25 19:56:24 +0000

    lagað

--- D7.py
+++ D7.py
@@ -4,21 +4,12 @@ import numpy as np
 
 tol =0.001
 
-<<<<<<< HEAD
-pi = np.pi
 #Hliðarlengdir Stewart-pallsins
 L1 = 3
 L2 = 3*np.sqrt(2)
 L3 = L1
+pi = np.pi
 gamma = pi/4
-=======
-
-def f(theta):
-    L1 = 3
-    L2 = 3*m.sqrt(2)
-    L3 = L1
-    gamma = m.pi/4
->>>>>>> 4be3147f6f8917c6fe0c718d8014e3cca7942739
 
 #Festipunktar tjakkanna
 x0 = 0
@@ -30,22 +21,12 @@ y1 = 0
 x2 = 0
 y2 = 6
 
-<<<<<<< HEAD
 def f(theta):
     #Hér þarf að finna þeta fyrst
     A2 = L3*np.cos(theta) - x1
     B2 = L3*np.sin(theta)
     A3 = L2*np.cos(theta+gamma) - x2
     B3 = L2*np.sin(theta+gamma) - y2
-=======
-    x2 = 0
-    y2 = 6
-
-    A2 = L3*m.cos(theta) - x1
-    B2 = L3*m.sin(theta)
-    A3 = L2*m.cos(theta+gamma) - x2
-    B3 = L2*m.sin(theta+gamma) - y2
->>>>>>> 4be3147f6f8917c6fe0c718d8014e3cca7942739
 
     #p1 er fastur sem 5
     p1 = 5
@@ -75,7 +56,7 @@ def bisection(a,b):
 
 p2 = 0
 
-space = -math.pi
+space = -m.pi
 answer = []
 counterB = 0
 
@@ -83,10 +64,10 @@ counterB = 0
 for i in range(0,100000):
 	counterA = 0
 	for j in range(0,100):
-		values = bisection(space, space+(math.pi)/50)
+		values = bisection(space, space+(m.pi)/50)
 		if values != None:
 			counterA += 1
-		space += (math.pi)/50
+		space += (m.pi)/50
 	if counterA != counterB:
 		answer.append(p2)
 	counterB = counterA

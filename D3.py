dcb671a46b91a47054e828203fcffccf593754df
Ívar Dór Orrason <ivardor@vr2t-dw025.rhi.hi.is>
2019-02-25 21:09:05 +0000

    Commenta JJ út úr D3

--- D3.py
+++ D3.py
@@ -1,6 +1,7 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
+
 #Fastar stærðir: L1, L2, L3, p1, p2, p3
 #Skilgreinum fasta utan fallsins:
 pi = np.pi
@@ -21,7 +22,45 @@ p1 = np.sqrt(5)
 p2 = p1
 p3 = p1
 
+def teiknatri( x_0, x_1, x_2, y_0, y_1, y_2, xb_0, xb_1, xb_2, yb_0, yb_1, yb_2):
+	#Þríhyrningurinn
+	plt.plot([x_0,x_1,x_2,x_0], [y_0,y_1,y_2,y_0], color='b')
+	plt.ylim(-1,5)
+	plt.xlim(-1,5)
 
+	#Línurnar útfrá þríhyrningnum
+	plt.plot([xb_0,x_0], [yb_0,y_0], color='b')
+	plt.plot([xb_1,x_1], [yb_1,y_1], color='b')
+	plt.plot([xb_2,x_2], [yb_2,y_2], color='b')
+
+	#Plotta punktana í þríhyrningnum
+	plt.scatter(x_0, y_0, c='b', s=20)
+	plt.scatter(x_1, y_1, c='b', s=20)
+	plt.scatter(x_2, y_2, c='b', s=20)
+
+	#Plotta punktana útfrá línunum
+	plt.scatter(xb_0, yb_0, c='b', s=20)
+	plt.scatter(xb_1, yb_1, c='b', s=20)
+	plt.scatter(xb_2, yb_2, c='b', s=20)
+
+	print("x hnit:")
+	print(x_0)
+	print("y hnit:")
+	print(y_0)
+
+	print("L1")
+	print(np.sqrt((x_1-x_2)**2+(y_1-y_2)**2))
+	print(L1)
+
+	print("L2")
+	print(np.sqrt((x_1-x_0)**2+(y_1-y_0)**2))
+	print(L2)
+
+	print("L3")
+	print(np.sqrt((x_2-x_0)**2+(y_2-y_0)**2))
+	print(L3)
+
+"""
 plt.figure("þríhyrningar 1")
 #Þríhyrningurinn
 plt.plot([1,2,2,1], [2,1,3,2], color='b')
@@ -64,3 +103,11 @@ plt.scatter(3, 2, c='b', s=20)
 plt.scatter(4, 0, c='b', s=20)
 plt.scatter(0, 0, c='b', s=20)
 plt.scatter(0, 4, c='b', s=20)
+"""
+
+plt.figure("Dæmi 3: Graf 1")
+teiknatri(1,2,2, 2,3,1, 0,0,4, 0,4,0)
+
+plt.figure("Dæmi 3: Graf 2")
+teiknatri(2,1,3, 1,2,2, 0,0,4, 0,4,0)
+plt.show()

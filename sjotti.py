b049186d8dd2a5e80b33d4cb929ff45055ca920f
Jessý Rún Jónsdóttir <jessy@vr2t-dw300.rhi.hi.is>
2019-02-25 14:57:44 +0000

    lagaglaglag

--- sjotti.py
+++ sjotti.py
@@ -1,33 +1,37 @@
 import math as m
 import matplotlib.pyplot as plt
 import numpy as np
+from numpy import sign
+
+pi = np.pi
+#Hliðarlengdir Stewart-pallsins
+L1 = 3
+L2 = 3*np.sqrt(2)
+L3 = L1
+gamma = pi/4
+
+#Festipunktar tjakkanna
+x0 = 0
+y0 = 0
+
+x1 = 5
+y1 = 0
+
+x2 = 0
+y2 = 6
 
 def f(theta):
-    pi = m.pi
-    #Hliðarlengdir Stewart-pallsins
-    L1 = 3
-    L2 = 3*m.sqrt(2)
-    L3 = L1
-    gamma = pi/4
-
-    #Festipunktar tjakkanna
-    x0 = 0
-    y0 = 0
-
-    x1 = 5
-    y1 = 0
-
-    x2 = 0
-    y2 = 6
-
     #Hér þarf að finna þeta fyrst
-    A2 = L3*m.cos(theta) - x1
-    B2 = L3*m.sin(theta)
-    A3 = L2*m.cos(theta+gamma) - x2
-    B3 = L2*m.sin(theta+gamma) - y2
+    A2 = L3*np.cos(theta) - x1
+    B2 = L3*np.sin(theta)
+    A3 = L2*np.cos(theta+gamma) - x2
+    B3 = L2*np.sin(theta+gamma) - y2
 
+    #p1 er fastur sem 5
     p1 = 5
-    p2 = p1
+    #Breytum p2 frá 1-10 og skoðum
+    p2 = 1
+    #p3 er fastur sem 3
     p3 = 3
 
     D = 2*(A2*B3 - B2*A3)
@@ -36,19 +40,23 @@ def f(theta):
     N2 = -A3*(p2**2-p1**2-A2**2-B2**2)+A2*(p3**2-p1**2-A3**2-B3**2)
     return N1**2 + N2**2 -p1**2*D**2
 
-    # Segjum stærð á grid og á hvaða bili (-pi til pi)
-    xvals = np.arange(-np.pi, np.pi, 0.01)
-    yvals = f(xvals)
-    #búum til plottið
-    plt.plot(xvals, yvals)
+def teiknagrafbil(titill, xnedra, xefra, ynedra, yefra):
+    xvals = np.arange(-np.pi, np.pi, 0.01) # Grid of 0.01 spacing from pi to pi
+    yvals = f(xvals) # Evaluate function on xvals
+    plt.plot(xvals, yvals) #Create line plot with yvals against xvals
 
-    #plt.xlim(0, 100) #Takmörk ásanna
-    #plt.ylim(0, 100)
+    plt.xlim(xnedra, xefra) #Takmörk ásanna
+    plt.ylim(ynedra, yefra)
 
-	#Merkjum grafið
+	#merkjum grafið
     plt.grid(True)
     plt.title(titill)
     plt.xlabel('Theta')
     plt.ylabel('Fallgildi')
 
-    plt.show()
+plt.figure("Graf 6.1: Stækkun")
+teiknagrafbil("Graf 6.1: Stækkun", -1, 2.7,-5,5 )
+plt.show()
+
+import math as m
+from numpy import sign

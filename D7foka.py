62a12d97925e10a0d6c57684f7f76ed22a4a7304
Ívar Dór Orrason <ivardor@vr2t-dw025.rhi.hi.is>
2019-02-25 20:35:39 +0000

    hæ

--- D7foka.py
+++ D7foka.py
@@ -72,3 +72,24 @@ for i in range(0,10000):
     p2 += 0.001
 
 print(answer)
+
+def teiknagraf(titill):
+	xvals = np.arange(-np.pi, np.pi, 0.01) # Grid of 0.01 spacing from pi to pi
+	yvals = f(xvals) # Evaluate function on xvals
+	plt.plot(xvals, yvals) #Create line plot with yvals against xvals
+
+	#plt.xlim(0, 100) #Takmörk ásanna
+	#plt.ylim(0, 100)
+
+	#merkjum grafiðs
+	plt.grid(True)
+	plt.title(titill)
+	plt.xlabel('Theta')
+	plt.ylabel('Fallgildi')
+	#plt.show()
+
+for i in range (0,5):
+    p2 = answer[i]
+    plt.figure("i")
+    teiknagraf("i")
+    plt.show()

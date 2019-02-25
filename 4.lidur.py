6bb94ef373cdb5a8f6568a35fe19aea4628d9307
Ívar Dór Orrason <ivardor@vr2t-dw025.rhi.hi.is>
2019-02-25 16:14:46 +0000

    hæææ

--- 4.lidur.py
+++ 4.lidur.py
@@ -1,9 +1,7 @@
 import numpy as np
 import matplotlib.pyplot as plt
-import Teiknagraf.py*
-import Teiknamynd.py*
 
-#Breytum föstunum til að teikna í 4.lið
+#Breytum föstunum til að þeir samræmist 4. lið
     #Hliðarlengdir Stewart-pallsins
 L1 = 3
 L2 = 3*np.sqrt(2)
@@ -32,10 +30,43 @@ y_1 = 5
 x_2 = 5
 y_2 = 5
 
-plt.figure("Graf 2")
-teiknagraf("Graf 2")
+def teiknamynd( x_0, x_1, x_2, y_0, y_1, y_2, xb_0, xb_1, xb_2, yb_0, yb_1, yb_2):
+	#Þríhyrningurinn
+	plt.plot([x_0,x_1,x_2,x_0], [y_0,y_1,y_2,y_0], color='b')
+	plt.ylim(-2,7)
+	plt.xlim(-2,7)
 
-def xogy(theta):
+	#Línurnar útfrá þríhyrningnum
+	plt.plot([xb_0,x_0], [yb_0,y_0], color='b')
+	plt.plot([xb_1,x_1], [yb_1,y_1], color='b')
+	plt.plot([xb_2,x_2], [yb_2,y_2], color='b')
+
+	#Plotta punktana í þríhyrningnum
+	plt.scatter(x_0, y_0, c='b', s=20)
+	plt.scatter(x_1, y_1, c='b', s=20)
+	plt.scatter(x_2, y_2, c='b', s=20)
+
+	#Plotta punktana útfrá línunum
+	plt.scatter(xb_0, yb_0, c='b', s=20)
+	plt.scatter(xb_1, yb_1, c='b', s=20)
+	plt.scatter(xb_2, yb_2, c='b', s=20)
+
+def teiknagraf(titill):
+	xvals = np.arange(-np.pi, np.pi, 0.01) # Grid of 0.01 spacing from pi to pi
+	yvals = f(xvals) # Evaluate function on xvals
+	plt.plot(xvals, yvals) #Create line plot with yvals against xvals
+
+	#plt.xlim(0, 100) #Takmörk ásanna
+	#plt.ylim(0, 100)
+
+	#merkjum grafiðs
+	plt.grid(True)
+	plt.title(titill)
+	plt.xlabel('Theta')
+	plt.ylabel('Fallgildi')
+	#plt.show()
+
+def teiknarot(theta):
 	#Reiknum A og B
 	A2 = L3*np.cos(theta) - x1
 	B2 = L3*np.sin(theta)
@@ -52,30 +83,32 @@ def xogy(theta):
 	print(x_0)
 	print(y_0)
 
-	if(theta+gamma > (np.pi/2)):
-		x_1 = x_0 - L1*np.sin(theta+gamma-(np.pi/2))
-		y_1 = y_0 + L1*np.sin(theta+gamma)
-	else:
-		x_1 = x_0 + L1*np.cos(theta+gamma)
-		y_1 = y_0 + L1*np.sin(theta+gamma)
+	x_1 = x_0 + L1*np.cos(theta+gamma)
+	y_1 = y_0 + L1*np.sin(theta+gamma)
 
-	if(gamma > (np.pi/2)):
-		x_2 = x_0 - L1*np.cos(theta-(np.pi/2))
-		y_2 = y_0 + L1*np.sin(theta)
-	else:
-		x_2 = x_0 + L1*np.cos(theta)
-		y_2 = y_0 + L1*np.sin(theta)
+	x_2 = x_0 + L1*np.cos(theta)
+	y_2 = y_0 + L1*np.sin(theta)
 	teiknamynd( x_0, x_1, x_2, y_0, y_1, y_2 , 0, 0, 5, 0, 6, 0)
+
+
+
+
+
+plt.figure("Graf 2")
+teiknagraf("Graf 2")
 
 
 plt.figure("Dæmi 4")
 plt.subplot(221)
-xogy(-0.7208)
+teiknarot(-0.7208)
+
 plt.subplot(222)
-xogy(-0.3310)
+teiknarot(-0.3310)
+
 plt.subplot(223)
-xogy(1.1437)
+teiknarot(1.1437)
+
 plt.subplot(224)
-xogy(2.1160)
+teiknarot(2.1160)
 
 #er ehv heima hallo ?
